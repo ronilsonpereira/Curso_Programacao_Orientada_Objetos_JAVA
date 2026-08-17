e0bce8709442ac5499ccdda750355d9ea0e5eeed
Ronilson Pereira <ronilsonengenharia@gmail.com>
2026-08-17 15:34:40 -0300

    colocando cod da aula01

---
+++ Codigos_Aula/Main.java
@@ -0,0 +1,52 @@
+//import java.util.Scanner; 
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// Primeiro programa em JAVA
+		
+		//System.out.println("Olá mundo, esse é meu primeiro programa em JAVA");
+		
+		
+		// ####################################
+		// Iniciando duas variaveis e mostrando o resultado da Média entre as duas.
+		
+		//float num1 = 3;
+		//float num2 = 6;
+		//float media = (num1 + num2) / 2; // testar sem a criação da variavel "media"
+				
+		//System.out.println(media); \\ (num1+num2)/2 
+		
+		
+		// ####################################
+		// fazendo a leitura de dois números digitados pelo usuário
+		
+		
+		// Scanner sc = new Scanner(System.in);
+		
+		//float num1;
+		//float num2;
+		
+		//System.out.println("Digite o primeiro número: ");
+		
+		//num1 = sc.nextFloat();
+		
+		//System.out.println("Digite o segundo número: ");
+		
+		//num2 = sc.nextFloat();
+		
+		// imprimindo usando o println
+		//System.out.println("A média de " + num1 + " mais " + num2 + " é igual a " + ((num1+num2)/2) );
+		
+		// imprimir usando o printf
+		//System.out.printf("A média de %.2f mais %.2f é igual a %.2f", num1, num2, ((num1+num2)/2) );
+		
+		
+		
+		
+		
+		
+
+	}
+}
