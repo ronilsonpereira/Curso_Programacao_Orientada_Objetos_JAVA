fcd2f4b4a9d5385ddf1b69322161c374ebc438c4
Ronilson Pereira <ronilsonengenharia@gmail.com>
2026-08-27 12:32:15 -0300

    Cod Aula 2

---
+++ Codigos_Aula/Aula2.java
@@ -0,0 +1,80 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner (System.in);
+
+
+		// Programa do Slide 49
+		
+		//String x;
+		//int y;
+		//double z;
+		
+		// lendo tudo na mesma linha, sem digitar o enter
+		
+		// Maria 30 35.5
+		
+		//x = sc.next(); // faz a leitura de apenas uma palavra
+		//y = sc.nextInt();
+		//z = sc.nextDouble();		
+		
+		//System.out.println(x); // Maria
+		//System.out.println(y); // 30
+		//System.out.println(z); // 35.5
+		
+		// Ou fazer tudo em apenas um System usando o println
+		
+		//System.out.println(x + " " + y + " "+ z ); // Maria
+		
+		//ou usando o printf
+		
+		//System.out.printf("%s %d %.1f", x, y, z);
+
+
+
+
+
+		
+		// Programa do Slide 51
+		
+		//int  x;
+		//String s1, s2, s3;
+		//x = sc.nextInt(); // Lendo um número inteiro
+		//sc.nextLine(); // Para consumir o enter após a leitura de X
+		//s1 = sc.nextLine(); // Bom dia
+		//s2 = sc.nextLine(); // Boa tarde
+		//s3 = sc.nextLine(); // Boa noite
+		
+		//System.out.println("Dados digitados: ");
+		//System.out.println(x);
+		//System.out.println(s1);
+		//System.out.println(s2);
+		//System.out.println(s3);
+
+
+		// Operações
+
+		
+		//int a = 3;
+		//int b = 2; // trocar por float = 2.0 
+		
+		//float eq = (a + b) /2;
+		// ou usar o casting
+		//float eq = (float) (a + b) /2;
+		
+		//System.out.println(eq);
+		
+		
+		
+		
+		
+	
+	}
+
+}
